4421fd2b152033d655f91535e98b1fff14c80dba
Nimsara Thamel <nimsarathamel@gmail.com>
2025-10-05 15:44:13 +0530

    Remove unnecessary blank line in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-
 import 'data/contacts_repository.dart';
 import 'firebase_options.dart';
 
